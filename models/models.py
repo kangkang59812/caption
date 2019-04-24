46a38277afb4ddcf45c9f2260a685928ed7412a8
kangkang <596286458@qq.com>
2019-04-24 21:06:06 +0800

    complete model

--- models/models.py
+++ models/models.py
@@ -7,14 +7,15 @@
 import torch
 import torch.nn as nn
 import torchvision
+from torchsummary import summary
 
-print('aaa')
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
 class Encoder(nn.Module):
     """
     Encoder.
     """
-
-    def __init__(self, basemodel='vgg16', encoded_image_size=14):
+    def __init__(self, basemodel='res101', encoded_image_size=14):
         super(Encoder, self).__init__()
         self.enc_image_size = encoded_image_size
         self.basemodel = basemodel
@@ -71,8 +72,42 @@ class Encoder(nn.Module):
             for p in c.parameters():
                 p.requires_grad = fine_tune
 
+class Attention(nn.Module):
+    """
+    Attention Network.
+    """
 
-encoder = Encoder()
-d = torch.randn(1, 3, 512, 512)
+    def __init__(self, encoder_dim, decoder_dim, attention_dim):
+        """
+        :param encoder_dim: feature size of encoded images
+        :param decoder_dim: size of decoder's RNN
+        :param attention_dim: size of the attention network
+        """
+        super(Attention, self).__init__()
+        self.encoder_att = nn.Linear(encoder_dim, attention_dim)  # linear layer to transform encoded image
+        self.decoder_att = nn.Linear(decoder_dim, attention_dim)  # linear layer to transform decoder's output
+        self.full_att = nn.Linear(attention_dim, 1)  # linear layer to calculate values to be softmax-ed
+        self.relu = nn.ReLU()
+        self.softmax = nn.Softmax(dim=1)  # softmax layer to calculate weights
+
+    def forward(self, encoder_out, decoder_hidden):
+        """
+        Forward propagation.
+
+        :param encoder_out: encoded images, a tensor of dimension (batch_size, num_pixels, encoder_dim)
+        :param decoder_hidden: previous decoder output, a tensor of dimension (batch_size, decoder_dim)
+        :return: attention weighted encoding, weights
+        """
+        att1 = self.encoder_att(encoder_out)  # (batch_size, num_pixels, attention_dim)
+        att2 = self.decoder_att(decoder_hidden)  # (batch_size, attention_dim)
+        att = self.full_att(self.relu(att1 + att2.unsqueeze(1))).squeeze(2)  # (batch_size, num_pixels)
+        alpha = self.softmax(att)  # (batch_size, num_pixels)
+        attention_weighted_encoding = (encoder_out * alpha.unsqueeze(2)).sum(dim=1)  # (batch_size, encoder_dim)
+
+        return attention_weighted_encoding, alpha
+
+encoder = Encoder().to(device)
+d = torch.randn(1, 3, 512, 512).cuda()
 out = encoder(d)
 print(out.shape)
+summary(encoder,(3,512,512))
